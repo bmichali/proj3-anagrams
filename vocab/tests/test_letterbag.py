95e9d81ffb3fe983b1b0afd41682180af254f9d8
bmichali <32374359+bmichali@users.noreply.github.com>
2017-10-16 16:42:25 -0700

    Update test_letterbag.py

--- vocab/tests/test_letterbag.py
+++ vocab/tests/test_letterbag.py
@@ -43,16 +43,16 @@ def test_str_with_caps():
     """
     A test for seeing if caps are downcased properly.
     """
-    assert str(LetterBag("ABCXYZ")) == "abcxyz"
-    assert str(LetterBag("AaBbcd")) == "aabbcd"
+    assert str(LetterBag("ABCXYZ")) == "ABCXYZ"
+    assert str(LetterBag("AaBbcd")) == "AaBbcd"
     
 def test_contains_with_caps():
     """
     A test for seeing if caps are downcased, 
     and .contains() works with cap'd str's.
     """
-    assert LetterBag("abcxyz").contains("ABCXYZ")
-    assert LetterBag("aabbcd").contains("AaBbcd")
+    assert not LetterBag("ABCXYZ").contains("ABCXYZ")
+    assert not LetterBag("aabbcd").contains("AaBbcd")
 
 def test_simple_merge():
     bag_abbc = LetterBag("abbc")
